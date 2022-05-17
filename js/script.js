e09e745afad07996d7b9e4ae31d8936575f0f304
Anton <art-medovikov@yandex.ru>
2022-05-17 17:01:30 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -5,19 +5,35 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
         tabs.forEach(item =>{
             item.classList.remove('tabheader__item_active')
         });
     }
 
-    function showTabContent(i) {
-        tabsContent[i].style.display = 'block';
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide')
+
         tabs[i].classList.add('tabheader__item_active');
     }
 
     hideTabContent();
-    showTabContent(0);
+    showTabContent();
+
+    tabParent.addEventListener('click', (event) =>{
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    })
 });
 
